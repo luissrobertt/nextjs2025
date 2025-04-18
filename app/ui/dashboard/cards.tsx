7f8bd120e9830e2d84c13233b6fdbdc3078e64c7
Luis  Garcia <73622504+luissrobertt@users.noreply.github.com>
2025-04-18 16:52:52 -0600

    actualizando a paso 10

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,3 +1,6 @@
+import { fetchCardData } from '@/app/lib/data';
+// import { CardWrapper } from '@/app/ui/dashboard/cards'; // ELIMINA ESTA LÍNEA
+
 import {
   BanknotesIcon,
   ClockIcon,
@@ -13,23 +16,23 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
+// La función CardWrapper obtiene los datos, pero no se renderiza directamente
 export default async function CardWrapper() {
-  return (
-    <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      /> */}
-    </>
-  );
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+  return {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  };
 }
 
+// Este es el componente que renderiza cada tarjeta individual
 export function Card({
   title,
   value,
